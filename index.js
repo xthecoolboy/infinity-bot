ce904c78ae07d1481d2cfbbc227d7c6e15a03083
Nicholas Nhien <eodc@users.no.reply.com>
2017-03-31 13:21:35 -0700

    Ping pong

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ var bot = new Discord.Client({
 bot.on('ready', function (event) {
   console.log('Logged in as %s - %s\n', bot.username, bot.id)
 })
+
+bot.on('message', function (user, userID, channelID, message, event) {
+  if (message === 'ping') {
+    bot.sendMessage({
+      to: channelID,
+      message: 'pong'
+    })
+  }
+})
