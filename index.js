ea258f6a58f5f1268cd034da1af3ea07a3146180
eodc <eodc@users.no.reply.com>
2017-04-01 18:52:29 -0700

    Fix Voice Connection Bugs

--- index.js
+++ index.js
@@ -12,24 +12,38 @@ client.on('message', message => {
     console.log('Sent Pong to ' + message.author.username)
   }
 })
-
 client.on('message', message => {
   if (message.content.toUpperCase() === config.prefix + 'JV') {
-    const channel = message.member.voiceChannel
-    channel.join().then(connection => {
-      console.log(message.author.username + ' summoned bot to ' + message.member.voiceChannel.name)
-      connection.playFile('./music/Braken - To The Stars.mp3')
-      message.channel.sendMessage('**I\'m now connected to: __' + message.member.voiceChannel.name + '__**\nCurrently, all I can do is play Braken\'s To the Stars. You can make me leave by typing -lv')
-    .catch(console.error)
-    })
+    const senderChannel = message.member.voiceChannel
+    if (senderChannel !== undefined) {
+      const channel = message.member.voiceChannel
+      channel.join().then(connection => {
+        console.log(message.author.username + ' summoned bot to ' + message.member.voiceChannel.name)
+        connection.playFile('./music/Braken - To The Stars.mp3')
+        message.channel.sendMessage('**I\'m now connected to: __' + message.member.voiceChannel.name + '__**\nCurrently, all I can do is play Braken\'s To the Stars. You can make me leave by typing ' + config.prefix + 'lv')
+      .catch(console.error)
+      })
+    }
+    else if (senderChannel === undefined) {
+      message.channel.sendMessage(message.member.user + ', you\'re not connected to a voice channel!')
+    }
   }
 })
-
 client.on('message', message => {
   if (message.content.toUpperCase() === config.prefix + 'LV') {
-    const channel = message.member.voiceChannel.connection
-    channel.disconnect()
-    console.log('Leaving voice...')
+    const senderChannel = message.member.voiceChannel
+    const botVoiceChannel = client.voiceConnections.first().channel
+    if (senderChannel !== undefined && senderChannel === botVoiceChannel) {
+      const channel = message.member.voiceChannel.connection
+      channel.disconnect()
+      console.log('Leaving voice...')
+    }
+    else if (senderChannel === undefined) {
+      message.channel.sendMessage(message.member.user + ', you\'re not connected to a voice channel!')
+    }
+    else if (senderChannel !== botVoiceChannel) {
+      message.channel.sendMessage(message.member.user + ', you\'re not in the same channel as me!')
+    }
   }
 })
 
