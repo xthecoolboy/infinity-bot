80f32ea8ec99eff2e2b56cbb190b5d07f1dfd03f
eodc <nicholasnhien@gmail.com>
2017-06-17 16:56:50 -0700

    Add ability to do 'd20' for dice roll

--- commands/misc/roll.js
+++ commands/misc/roll.js
@@ -1,5 +1,4 @@
 const { Command } = require('discord.js-commando')
-const { stripIndents } = require('discord.js-commando')
 
 module.exports = class RollCommand extends Command {
   constructor (client) {
@@ -12,20 +11,22 @@ module.exports = class RollCommand extends Command {
         'roll',
         'roll 10',
         'roll 20'
-      ],
-      args: [
-        {
-          key: 'size',
-          default: '6',
-          prompt: '',
-          type: 'integer'
-        }
       ]
     })
   }
   async run (msg, args) {
-    const die = args.size
-    const roll = Math.floor(Math.random() * die + 1)
-    msg.channel.send(`${msg.member.user} rolled a ${roll}`)
+    if (args.toLowerCase().startsWith('d')) {
+      const dice = args.slice(1)
+      const roll = Math.floor(Math.random() * dice + 1)
+      if (isNaN(roll)) return msg.reply(`invalid input...`)
+      return msg.channel.send(`${msg.member.user} rolled a ${roll}`)
+    }
+    if (!args) {
+      const roll = Math.floor(Math.random() * 6 + 1)
+      return msg.channel.send(`${msg.member.user} rolled a ${roll}`)
+    }
+    const roll = Math.floor(Math.random() * args + 1)
+    if (isNaN(roll)) return msg.reply(`invalid input...`)
+    return msg.channel.send(`${msg.member.user} rolled a ${roll}`)
   }
 }
